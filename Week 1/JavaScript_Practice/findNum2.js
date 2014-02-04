6a16a32610432f71b18c9ae89e9186bc8be7dc96
jaimi kunz <jaimikunz@gmail.com>
2014-02-04 14:25:35 -0600

    Starting a New Class, With all new Files

--- Week 1/JavaScript_Practice/findNum2.js
+++
@@ -1,9 +0,0 @@
-var arr1 = [1,2,3,4,5,6,7,8,9];
-var arr2 =['a','b','c','d','e','f'];
-var arr3 = ['apple','bananana', 5672, 5.327];
-
-console.log(arr2.indexOf('e'));
-
-
-
-console.log(arr2.join(','));
