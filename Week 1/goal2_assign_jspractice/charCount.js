443d0e0a5462616aaecc09bb37050eafafe72158
jaimi kunz <jaimikunz@gmail.com>
2014-03-06 19:15:56 -0600

    JavaScript Practice assignment

---
+++ Week 1/goal2_assign_jspractice/charCount.js
@@ -0,0 +1,11 @@
+function
+
+if(sunny){goPlayatTheBeach();
+} else{
+	if(warmWater){wearNewSuit();
+} else{
+
+ (goToThePark();
+}
+
+console.log("goPlayatTheBeach!")
---
+++ Week 1/goal2_assign_jspractice/findNum.js
@@ -0,0 +1,18 @@
+var num1 = 15;
+var num2 = 10;
+num1 += num2;
+console.log ("+=: ", num1);
+
+
+
+var myStr = "10" + 6;
+console.log ("Number + Strings: ", myStr);
+
+
+
+
+var num = 2 + (5 * 5 - 8) / 5;
+console.log ("order of operations: ", num);
+
+
+
